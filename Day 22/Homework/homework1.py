6886322544b86e9d703fb69a87d3cc44294840e2
MUMLA000 <sabamumladze2009@gmail.com>
2025-05-13 21:59:36 +0400

    commit 1

---
+++ Day 22/Homework/homework1.py
@@ -0,0 +1,6 @@
+vegetables = ["სტაფილო", "ბადრიჯანი", "კომბოსტო", "ბოლოკი"]
+
+vegetables[1] = "კიტრი"
+vegetables[3] = "პამიდორი"
+
+print(vegetables)
---
+++ Day 22/Homework/homework2.py
@@ -0,0 +1,5 @@
+vegetables = ["სტაფილო", "ბადრიჯანი", "კომბოსტო", "ბოლოკი"]
+
+user_choice = int(input("აირჩიეთ რიცხვი 0-დან 3-მდე: "))
+
+print("თქვენი არჩევანი:", vegetables[user_choice])
